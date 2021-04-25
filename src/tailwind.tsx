f56d3129c0c9ad05f93671b63a11dd5966716d79
Marisa Clardy <marisa@clardy.eu>
2021-04-25 18:56:35 -0500

    Add more type safety. This ensures that the template elements are only prop functions, and it makes the tag function generic, to specify the props for it.

--- src/tailwind.tsx
+++ src/tailwind.tsx
@@ -20,7 +20,7 @@ const cleanTemplate = (template: TemplateStringsArray, inheritedClasses: string 
     ).split(" ")
 }
 
-function parseTailwindClassNames(template: string[], ...templateElements: any[]) {
+function parseTailwindClassNames(template: string[], ...templateElements: (string | undefined | null)[]) {
     return template
         .reduce((sum, n, index) => {
             const templateElement = templateElements[index]
@@ -33,31 +33,30 @@ function parseTailwindClassNames(template: string[], ...templateElements: any[])
         .replace(/\s{2,}/g, " ") // replace line return by space
 }
 
-export type FunctionTemplate<P, E> = (
+export type FunctionTemplate<P, E> = <K extends {} = {}>(
     template: TemplateStringsArray,
-    ...templateElements: any[]
-) => React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<E>>
+    ...templateElements: ((props: P & K) => (string | undefined | null))[]
+) => React.ForwardRefExoticComponent<React.PropsWithoutRef<P & K> & React.RefAttributes<E>>
 
 interface ClassNameProp { className?: string }
-type Component<Props extends ClassNameProp> = React.ComponentType<Props>
-
-function functionTemplate<P extends ClassNameProp, E = unknown>(Element: Component<P>): FunctionTemplate<P, E> {
-    return (template, ...templateElements) => React.forwardRef<E, P>(({ children, ...props }, ref) => (
+function functionTemplate<P extends ClassNameProp, E = any>(Element: React.ComponentType<P>): FunctionTemplate<P, E> {
+    return <K extends {}>(
+        template: TemplateStringsArray,
+        ...templateElements: ((props: P & K) => (string | undefined | null))[]
+    ) => React.forwardRef<E, P & K>((props, ref) => (
         <Element
-            {...props as P}
+            {...props}
             ref={ref}
             className={parseTailwindClassNames(
                 cleanTemplate(template, props.className),
                 ...templateElements.map((t) => t(props))
             )}
-        >
-            {children}
-        </Element>
+        />
     ))
 }
 
 export type IntrinsicElements = {
-    [key in keyof JSX.IntrinsicElements]: FunctionTemplate<React.ComponentProps<key>, JSX.IntrinsicElements[key]>
+    [key in keyof JSX.IntrinsicElements]: FunctionTemplate<JSX.IntrinsicElements[key], key>
 }
 
 const intrinsicElements: IntrinsicElements = domElements.reduce(
