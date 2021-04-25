750b846038b1952aa8b9568c22c3e4bc0b6ff7b5
Marisa Clardy <marisa@clardy.eu>
2021-04-25 17:32:37 -0500

    rewrite tests to be a bit more accurate. The snapshots were regenerated with the original code.

--- src/__tests__/tailwind.test.tsx
+++ src/__tests__/tailwind.test.tsx
@@ -2,28 +2,33 @@ import React from 'react'
 import tw from '../tailwind'
 import { render } from '@testing-library/react';
 
+interface TestCompProps extends React.PropsWithChildren<any> {
+  className?: string;
+  someThingElse?: string;
+}
+
 describe('tw', () => {
   it('matches snapshot with intrinsic element', () => {
     const Div = tw.div`bg-gray-400`;
-    const { asFragment } = render(<Div />);
+    const { asFragment } = render(<Div>test</Div>);
     expect(asFragment()).toMatchSnapshot();
   })
 
   it('matches snapshot with function component', () => {
-    const TestComp = () => <div>Test</div>
+    const TestComp = ({ className = '', children }: TestCompProps) => <div className={className}>{children}</div>
     const TestCompStyled = tw(TestComp)`bg-gray-400`;
-    const { asFragment } = render(<TestCompStyled />);
+    const { asFragment } = render(<TestCompStyled>test</TestCompStyled>);
     expect(asFragment()).toMatchSnapshot();
   })
 
   it('matches snapshot with class component', () => {
-    class TestComp extends React.Component {
+    class TestComp extends React.Component<TestCompProps> {
       render() {
-        return <div>Test</div>
+        return <div className={this.props.className}>{this.props.children}</div>
       }
     }
     const TestCompStyled = tw(TestComp)`bg-gray-400`;
-    const { asFragment } = render(<TestCompStyled />);
+    const { asFragment } = render(<TestCompStyled>test</TestCompStyled>);
     expect(asFragment()).toMatchSnapshot();
   })
 })
