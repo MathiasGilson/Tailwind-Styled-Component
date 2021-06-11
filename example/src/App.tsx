c439c3016f56e16c83a9be01bd3f9eb0bde532a7
Mathias Gilson <mth.glsn@gmail.com>
2021-06-11 13:47:42 +0100

    minor fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -13,6 +13,22 @@ export default () => {
         <Container>
             <Title $bold={false}>Hello</Title>
             <Input onChange={onChange} onFocus={() => console.log(`focus`)} value={value} />
+
+            <DefaultContainer>
+                <pre>
+                    DefaultContainer
+                    <br />I have following classes: flex items-center bg-blue-600
+                </pre>
+            </DefaultContainer>
+            <RedContainer>
+                <pre>
+                    Red Container
+                    <br />
+                    I have following classes: bg-red-300 flex items-center bg-blue-600
+                    <br />
+                    but I should have: bg-red-300 flex items-center
+                </pre>
+            </RedContainer>
         </Container>
     )
 }
@@ -22,6 +38,16 @@ const Input = tw.input``
 const Container = tw.div`
     flex
     bg-blue-200
+`
+
+const DefaultContainer = tw.div`
+    flex
+    items-center
+    bg-blue-600
+`
+
+const RedContainer = tw(DefaultContainer)`
+    bg-red-300
 `
 
 const Title = tw.div<{ $bold: boolean }>`
