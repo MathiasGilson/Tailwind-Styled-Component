6484535c02b9575422e543f1eb0a840a9db959f4
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-19 23:43:27 +0100

    make node modules external in esbuild bundle

--- esbuild.js
+++ esbuild.js
@@ -1,8 +1,18 @@
-require("esbuild").buildSync({
+const makeAllPackagesExternalPlugin = {
+    name: "make-all-packages-external",
+    setup(build) {
+        const filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ // Must not start with "/" or "./" or "../"
+        build.onResolve({ filter }, (args) => ({ path: args.path, external: true }))
+    }
+}
+
+require("esbuild").build({
     entryPoints: ["./src/index.ts"],
+    // format: "esm",
     bundle: true,
     minify: true,
     sourcemap: true,
     target: ["chrome58", "firefox57", "safari11", "edge18"],
-    outfile: "build/index.js"
+    outfile: "build/index.js",
+    plugins: [makeAllPackagesExternalPlugin]
 })
